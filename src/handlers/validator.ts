df76d8ba09716af82c1a76284afbe2be2fd33d24
Matthew Bare <69798023+Animatted@users.noreply.github.com>
2023-04-16 17:38:26 -0600

    cleanup

--- src/handlers/validator.ts
+++ src/handlers/validator.ts
@@ -1,7 +1,7 @@
 /*
 This is a simple input validator function. It takes an input string and returns
 true if the input has no special characters and false if it does contain special
-characters. This function is used anywere a user provides data.
+characters. This function is used anywhere a user provides data.
 */
 
 
